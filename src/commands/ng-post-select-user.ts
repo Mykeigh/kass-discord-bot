7fe16f300e96a90d76cd769d295684e5b7e3ef11
Mykeigh <mykeixwolfe@gmail.com>
2021-09-05 21:07:50 -0500

    Updates select-menu

--- src/commands/ng-post-select-user.ts
+++ src/commands/ng-post-select-user.ts
@@ -12,40 +12,42 @@ export = {
     .setName("ngbbs")
     .setDescription("Get a random bbs post from a fellow NGer 🆖"),
   async execute(interaction: CommandInteraction) {
-    const row = new MessageActionRow()
-			.addComponents(
-				new MessageSelectMenu()
-					.setCustomId('ng-post-select')
-					.setPlaceholder('Select a User')
-					.addOptions([
-						{
-							label: 'Chdonga 🦶',
-							description: 'Feet Scuker 🐾',
-							value: 'Chdonga',
-						},
-						{
-              label: 'MemeFiend ♿',
-							description: '🥥',
-							value: 'MemeFiend',
-						},
-						{
-              label: 'MykeiXWolfe 🐺',
-							description: 'Who?',
-							value: 'MykeiXWolfe',
-						},
-            {
-              label: 'HeatherAran ‼',
-              description: '🐟',
-              value: 'HeatherAran',
-            },
-            {
-              label: 'Homicide 🔪',
-              description: 'The living legend 🌆',
-              value: 'Homicide',
-            },
-					]),
-			);
+    const row = new MessageActionRow().addComponents(
+      new MessageSelectMenu()
+        .setCustomId("ng-post-select")
+        .setPlaceholder("Select a User")
+        .addOptions([
+          {
+            label: "Chdonga 🦶",
+            description: "Feet Scuker 🐾",
+            value: "Chdonga",
+          },
+          {
+            label: "MemeFiend ♿",
+            description: "🥥",
+            value: "MemeFiend",
+          },
+          {
+            label: "Omega 🧺",
+            description: "What a little bitch 😂",
+            value: "Alpha",
+          },
+          {
+            label: "HeatherAran ‼",
+            description: "🐟",
+            value: "HeatherAran",
+          },
+          {
+            label: "Homicide 🔪",
+            description: "The living legend 🌆",
+            value: "Homicide",
+          },
+        ])
+    );
 
-		await interaction.reply({ content: 'Choose someone to fetch a post from...', components: [row] });
+    await interaction.reply({
+      content: "Choose someone to fetch a post from...",
+      components: [row],
+    });
   },
 };
